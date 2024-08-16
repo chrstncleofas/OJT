8590bc5fc2c7451003f6cce2cf355dc84940e102
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-08-16 19:39:35 +0800

    NO_TICKET - Fix issues

--- main/settings.py
+++ main/settings.py
@@ -35,12 +35,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'app.apps.AppConfig',
+    'superapp.apps.SuperappConfig',
     'students.apps.StudentsConfig',
+    'storages',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('students.urls')),
-    path('adminSite/', include('app.urls')),
     path('admin/', admin.site.urls),
+    path('adminSite/', include('app.urls')),
+    path('super-admin-page/', include('superapp.urls')),
 ]
