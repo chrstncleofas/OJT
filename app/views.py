b9c24195b42bc033d955780de6d129f16ffb6dbd
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-10-04 15:37:06 +0800

    NO_TICKET - Revision 33

--- app/views.py
+++ app/views.py
@@ -456,6 +456,8 @@ def submittedRequirementOfStudents(request, id):
 
     return render(request, 'app/view-submitted-requirement.html', context)
 
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 def getTheSubmitRequirements(request):
     user = request.user
     admin = get_object_or_404(CustomUser, id=user.id)
@@ -467,16 +469,20 @@ def getTheSubmitRequirements(request):
     students = DataTableStudents.objects.filter(status='Approved', archivedStudents='NotArchive').order_by('id')
 
     if search_query:
-        students = students.filter(
-            Q(Firstname__icontains=search_query)
-        )
-    
+        students = students.filter(Q(Firstname__icontains=search_query))
+
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
         return render(request, 'app/grading.html', {'listOfStudents': students})
     
     # Pagination logic
     page = request.GET.get('page', 1)
+
+    # Ensure per_page is an integer with a default value of 10
     per_page = request.GET.get('per_page', 10)
+    try:
+        per_page = int(per_page) if per_page else 10
+    except ValueError:
+        per_page = 10
 
     paginator = Paginator(students, per_page)
 
@@ -492,6 +498,7 @@ def getTheSubmitRequirements(request):
         'firstName': firstName,
         'lastName': lastName
     })
+
 
 def studentInformation(request, id):
     user = request.user
