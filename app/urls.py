79e9b0bc2fa23f500e502a410412cec85e62104c
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-10-12 18:03:54 +0800

    NO_TICKET - Revise 02

--- app/urls.py
+++ app/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login', views.userLoginFunction, name='login'),
+    # path('', views.home, name='home'),
+    # path('login', views.userLoginFunction, name='login'),
     path('register/', views.register, name='register'),
     path('profile', views.profile, name='profile'),
     path('logout', views.logoutView, name='logout'),
